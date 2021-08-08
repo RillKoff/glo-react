ca15990ab3d6f32139f3bbdf7d02be5d1ee9af3b
Kirill <RillKoff@yandex.ru>
2021-08-08 17:20:16 +0300

    second task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {NavBar} from './Components/NavBar';
 import { Menu } from './Components/menu';
-import { GlobalStyle } from './Components/GlobalStyle'
+import { GlobalStyle } from './Components/GlobalStyle';
+import { ModalItem } from './Components/ModelItem';
 
 
 function App() {
+
+  const [openItem, setOpenItem] = React.useState(null);
+
+
   return (
     <>
     <GlobalStyle/>
     <NavBar/>
-    <Menu/>
+    <Menu setOpenItem={setOpenItem}/>
+    <ModalItem openItem={openItem} setOpenItem={setOpenItem}/>
     </>
   );
 }
